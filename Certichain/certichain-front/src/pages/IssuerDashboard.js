28890ce05199f847a9fafaa47889cb07b4d4f8a4
JuSmoke <jussmoke84@gmail.com>
2026-03-02 10:26:49 +0100

    BE-51 : Fix dependencies error

--- Certichain/certichain-front/src/pages/IssuerDashboard.js
+++ Certichain/certichain-front/src/pages/IssuerDashboard.js
@@ -9,14 +9,15 @@ const IssuerDashboard = () => {
   const [msg, setMsg] = useState({ type: '', text: '' }); 
   const [formData, setFormData] = useState({ nom: '', prenom: '', dateObtention: '', diplomeFile: null, course_name: '' });
 
-  const fetchMyDiplomas = async () => {
-    const res = await fetch(`/api/my-diplomas/?user_id=${userId}`);
-    const data = await res.json();
-    setMyDiplomas(data);
-  };
-
   useEffect(() => {
-    if (activeTab === 'list') fetchMyDiplomas();
+    if (activeTab === 'list') { 
+      const fetchMyDiplomas = async () => {
+        const res = await fetch(`/api/my-diplomas/?user_id=${userId}`);
+        const data = await res.json();
+        setMyDiplomas(data);
+        };
+      fetchMyDiplomas();
+    }
   }, [activeTab]);
 
   const handleSubmit = async (e) => {
