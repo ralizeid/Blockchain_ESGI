8abc72587247f7e95e5ce39beaf6f59ab182a040
Rayan <117828075+ralizeid@users.noreply.github.com>
2026-04-12 16:15:55 +0200

    Change email settings to use SSL on port 465

--- Certichain/certichain-back/config/settings.py
+++ Certichain/certichain-back/config/settings.py
@@ -23,8 +23,8 @@ _email_pass = os.getenv('EMAIL_HOST_PASSWORD', '').strip()
 if _email_user and _email_pass:
     EMAIL_BACKEND      = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST         = 'smtp.gmail.com'
-    EMAIL_PORT         = 587
-    EMAIL_USE_TLS      = True
+    EMAIL_PORT         = 465
+    EMAIL_USE_SSL      = True
     EMAIL_HOST_USER    = _email_user
     EMAIL_HOST_PASSWORD = _email_pass
 else:
